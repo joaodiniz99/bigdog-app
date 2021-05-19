794e43d7f6099128a3af248de624beda0a32de1e
João <joaopbddiniz@gmail.com>
2021-05-19 19:33:11 +0100

    Página Not Found semi-finalizada

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,26 @@
 <template>
-    <div>
-        <h1>Not Found</h1>
-    </div>
+    <b-container fluid class="text-center">
+        <b-row>
+            <b-col class="my-5">
+                <b-icon icon="signpost-split" scale="5" class="notFoundIcon"></b-icon>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col class="mb-4">
+                <h1>Página Não Encontrada!</h1>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col >
+                <router-link to="/" class="" title="Retornar para a página principal">
+                    <b-button variant="info">
+                        Página principal <b-icon icon="signpost" aria-hidden="true"></b-icon>
+                    </b-button>
+                </router-link>
+                
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
@@ -11,5 +30,19 @@ export default {
 </script>
 
 <style scoped>
+@keyframes notFoundEntry {
+    0% {
+        transform: translateY(0);
+    }
+    50% {
+        transform: translateY(-10px);
+    }
+    100% {
+        transform: translateY(0);
+    }
+}
 
+.notFoundIcon {
+    animation: notFoundEntry 2s 0s ease-in-out infinite;
+}
 </style>
